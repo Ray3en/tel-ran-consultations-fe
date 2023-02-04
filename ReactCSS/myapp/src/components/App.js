081491591310e2725bbaafbc58bba8bd23bddc11
ray3en <tigranlee@gmail.com>
2023-02-04 18:07:09 +0400

    4.03

---
+++ ReactCSS/myapp/src/components/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header/Header'
+
+
+function App(){
+  return(
+    <div className={'test'}>
+      <p>123</p>
+      <Header/>
+    </div>
+  )
+}
+
+
+export default App
---
+++ ReactCSS/myapp/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header(){
+  return(
+    <div className={'header'}>
+      123
+    </div>
+  )
+}
+
+
+export default Header
