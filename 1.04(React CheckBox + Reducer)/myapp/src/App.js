bff455641263adb1d7411f5df1b84810836ff8d9
ray3en <tigranlee@gmail.com>
2023-04-22 16:42:53 +0400

    22.04

--- 1.04(React CheckBox + Reducer)/myapp/src/App.js
+++ 1.04(React CheckBox + Reducer)/myapp/src/App.js
@@ -3,12 +3,18 @@ import { useDispatch, useSelector } from "react-redux";
 
 function App() {
 
-  const products = useSelector(store => store).filter(elem => elem.show)
-  console.log(products)
+  const products = useSelector(store => store).filter(elem => elem.show && elem.showPrice)
   const dispatch = useDispatch()
 
   function salesHandle(e){
     dispatch({type: 'SALES_CHECKBOX', payload: e.target.checked })
+  }
+
+  const filterByPrice = (e) => {
+      let data = Object.fromEntries(new FormData(e.target.parentNode))
+      data.min = (data.min === '') ? -Infinity : +data.min
+      data.max = (data.max === '') ? Infinity : +data.max
+      dispatch({type: 'FILTER_PRICE', payload: data})
   }
 
   return (
@@ -18,12 +24,17 @@ function App() {
           Отобразить товары со скидкой
           <input type='checkbox' onClick={salesHandle}/>
         </label>
+        <form onChange={filterByPrice}>
+          <input name="min" type="number" placeholder="От"/>
+          <input name='max' type="number" placeholder="До"/>
+        </form>
       </div>
       <div>
         {products.map(elem => 
-            <div key={elem.id}>
+            <div style={{border: '1px solid black', padding: '10px'}} key={elem.id}>
               <h2>{elem.name}</h2>
               <p>{''+elem.sales}</p>
+              <p>{elem.price}</p>
             </div>
         )}
       </div>
--- 1.04(React CheckBox + Reducer)/myapp/src/store/index.js
+++ 1.04(React CheckBox + Reducer)/myapp/src/store/index.js
@@ -1,16 +1,19 @@
 import {createStore} from 'redux'
 
 const defaultState = [
-    {id: 1, name: 'Велосипед', sales: true},
-    {id: 2, name: 'Самокат', sales: false},
-    {id: 3, name: 'Скейтборд', sales: false},
-    {id: 4, name: 'Ракетка', sales: true},
-    {id: 5, name: 'Перчатки', sales: false},
-    {id: 6, name: 'Коньки', sales: false},
+    {id: 1, price: 10,name: 'Велосипед', sales: true},
+    {id: 2, price: 20,name: 'Самокат', sales: false},
+    {id: 3, price: 30,name: 'Скейтборд', sales: false},
+    {id: 4, price: 60,name: 'Ракетка', sales: true},
+    {id: 5, price: 100,name: 'Перчатки', sales: false},
+    {id: 6, price: 180,name: 'Коньки', sales: false},
 ]
 
 const productReducer = (state = defaultState, action) => {
-    state = state.map(elem => ({...elem, show: true}))
+    
+    if(state[0].show == undefined){
+        state = state.map(elem => ({...elem, show: true, showPrice: true}))
+    }
 
     switch (action.type){
         case 'SALES_CHECKBOX':
@@ -24,9 +27,20 @@ const productReducer = (state = defaultState, action) => {
             } else {
                 return state.map(elem => ({...elem, show: true}))
             }
+        case 'FILTER_PRICE':
+            state = state.map(elem => ({...elem, showPrice: true}))
+            return state.map(elem => {
+                if (!(elem.price >= action.payload.min && elem.price <= action.payload.max)){
+                    elem.showPrice = false
+                }
+                return elem
+            })
+        // case 'SORT':
+        //     return state.slice().sort((a,b) => b - a)
         default:
             return state
     }
 }
 
 export const store = createStore(productReducer)
+
