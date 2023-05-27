0e98352e5d22743bd2d321902f2cc080ab74dac5
ray3en <tigranlee@gmail.com>
2023-05-27 15:38:26 +0300

    27.05

---
+++ 27.05.2023/JSON/script.js
@@ -0,0 +1,61 @@
+// const user = require('./data.json')
+// console.log(typeof user)
+
+// let obj = {
+//     name: 'Tigran',
+//     salary: 1000
+// }
+
+// JSON.stringify()     --  Преобразование JS объекта в JSON
+// JSON.parse()         --  Обратное преобразование JSON'a в JS объект
+
+// { name: 'Tigran', salary: 10 }           -- JS (object)
+// {"name":"Tigran","salary":1000}          -- JSON (string)
+
+// let json = JSON.stringify(obj)
+// console.log(JSON.parse(json))
+
+
+// ----------------------------------
+// Тернарный оператор
+// (condition) ? value1 : value2
+
+
+// if(...){
+//     console.log('YES')
+// } else {
+//     console.log('NO')
+// }
+
+
+// (condition) ? console.log('YES') : value2
+
+// let a = (condition) ? value1 : value2
+
+// {
+//     name: (condition) ? value1 : value2
+// }
+
+
+// console.log((10 < 100) ? 'YES' : 'NO')
+
+
+// if (10 < 100){
+//     console.log('YES')
+// } else {
+//     console.log('NO')
+// }
+
+// && ищет первый попавшийся false (или последний TRUE)
+// || ищет первый попавшйися true (или последний false)
+//          false   true
+// console.log(0 || 10 || 1 || -10 || '.')    --    ответ 10
+
+// ^ - циркумфлекс
+
+// Boolean() - функция конвертиурет почти все что угодно в булевый тип
+
+
+
+// https://habr.com/ru/articles/677236/                     -- slider
+// https://www.npmjs.com/package/react-image-gallery        -- аналог (библиотека)
